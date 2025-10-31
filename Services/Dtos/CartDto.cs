9d5a45742b88314347552fc9aa35405247e20822
machgiahao <machgiahao0979@gmail.com>
2025-10-31 16:29:29 +0700

    Fix error get cart

--- Services/Dtos/CartDto.cs
+++ Services/Dtos/CartDto.cs
@@ -5,6 +5,6 @@ namespace Services.Dtos;
 public class CartDto
 {
     public int CartId { get; set; }
-    public List<CartItemDto> Items { get; set; }
     public decimal TotalCartPrice { get; set; }
+    public List<CartItemDto> Items { get; set; } = new();
 }
