3ed4889ec8626d854ea37898cc12330ad7b57a4f
lhmy-eb <luonghongmy123456@gmail.com>
2025-11-14 00:06:18 +0700

    Add Get Store location

---
+++ Api/Controllers/StoreLocation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StoreLocation : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStoreLocation()
+        {
+            var location = new
+            {
+                Latitude = 10.875139,
+                Longitude = 106.800729,
+                Address = "Phòng 308 Lầu 3 Nhà Văn hóa Sinh viên TP.HCM, Số 1 Lưu Hữu Phước, Đông Hoà, Dĩ An, Thành phố Hồ Chí Minh, Việt Nam"
+            };
+            return Ok(location);
+        }
+    }
+}
